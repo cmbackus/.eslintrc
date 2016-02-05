70b1d4f0415e24374e0bc3ab465cd1632cfdc3ba
Charlie Backus <cmbackus@users.noreply.github.com>
2016-02-05 10:19:23 -0500

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2,
+            2
+        ],
+        "quotes": [
+            2,
+            "double"
+        ],
+        "linebreak-style": [
+            2,
+            "windows"
+        ],
+        "semi": [
+            2,
+            "always"
+        ]
+    },
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "extends": "eslint:recommended"
+};
